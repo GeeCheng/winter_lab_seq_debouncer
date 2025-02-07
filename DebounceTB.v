b669ed508a1c53a0eb8097bc5b409b84b66d72ae
黃俊誠 <s111060003@m111.nthu.edu.tw>
2025-02-08 03:20:19 +0800

    Update DebounceTB.v

--- DebounceTB.v
+++ DebounceTB.v
@@ -1,72 +1,66 @@
 `include "Debounce.v"
 
-`timescale  1ns / 1ps
+`timescale 1ns / 1ps
+`define CYCLE 10
 
 module DebounceTB;
 
+  /////////////////////////
+  // Signal Declarations //
+  /////////////////////////
+  reg  clk_i   = 1'b1;
+  reg  rst_ni  = 1'b0;
+  reg  raw_sig_i    = 1'b0;
+  wire debounce_sig_o;
 
-integer PERIOD  = 10;
+  //////////////////////
+  // Clock Generation //
+  //////////////////////
+  always #(`CYCLE/2) clk_i = ~clk_i;
 
-reg   clk    = 1 ;
-reg   rst_n  = 0 ;
-reg   sw     = 0 ;
+  ///////////////////////
+  // DUT Instantiation //
+  ///////////////////////
+  Debounce u_Debounce (
+    .clk_i            (clk_i),
+    .rst_ni           (rst_ni),
+    .raw_sig_i        (raw_sig_i),
+    .debounce_sig_o   (debounce_sig_o)
+  );
 
-wire  out                                  ;
+  ///////////////
+  // Test Task //
+  ///////////////
+  task hold_sw(input reg value, input integer cycles);
+    begin
+      repeat (cycles) @(negedge clk_i) raw_sig_i = value;
+    end
+  endtask
 
+  task toggle_sw(input integer cycles);
+    begin
+      repeat (cycles) @(negedge clk_i) raw_sig_i = ~raw_sig_i;
+    end
+  endtask
 
-initial begin
-    forever #(PERIOD/2)  clk=~clk;
-end
+  ////////////////////////
+  // Main Test Sequence //
+  ////////////////////////
+  initial begin
+    $dumpfile("DebounceTB.vcd");
+    $dumpvars(0, DebounceTB);
 
-initial begin
-  rst_n = 1;
-end
+    rst_ni = 1'b0;
+    #(2 * `CYCLE);
+    rst_ni = 1'b1;
 
-Debounce  u_Debounce (
-    .clk   ( clk     ),
-    .rst_n ( rst_n   ),
-    .sw    ( sw      ),
+    hold_sw(1'b0, 10);   // Hold raw_sig_i at 0 for 10 cycles
+    toggle_sw(10);       // Toggle raw_sig_i every period for 10 cycles
+    hold_sw(1'b1, 10);   // Hold raw_sig_i at 1 for 10 cycles
+    toggle_sw(6);        // Toggle raw_sig_i every period for 6 cycles
+    hold_sw(1'b0, 10);   // Hold raw_sig_i at 0 for 10 cycles
 
-    .out   ( out     )
-);
-
-integer i;
-
-initial
-begin
-  $dumpfile("DebounceTb.vcd");
-  $dumpvars(0, DebounceTB);
-
-  for (i = 0; i < 10; i = i + 1) begin
-    sw = 0;
-    #PERIOD;
+    $finish;
   end
-
-  for (i = 0; i < 5; i = i + 1) begin
-    sw = 0;
-    #PERIOD;
-    sw = 1;
-    #PERIOD;
-  end
-
-  for (i = 0; i < 10; i = i + 1) begin
-    sw = 1;
-    #PERIOD;
-  end
-
-  for (i = 0; i < 5; i = i + 1) begin
-    sw = 1;
-    #PERIOD;
-    sw = 0;
-    #PERIOD;
-  end
-
-  for (i = 0; i < 10; i = i + 1) begin
-    sw = 0;
-    #PERIOD;
-  end
-
-  $finish;
-end
 
 endmodule
